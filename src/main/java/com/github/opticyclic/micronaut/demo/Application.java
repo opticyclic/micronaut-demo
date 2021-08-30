6535772ff749abe07e996b5fc65b62a85a2755f6
opticyclic <opticyclic@gmail.com>
2021-08-30 15:39:08 -0400

    Add the basic outline of a gradle micronaut application

---
+++ src/main/java/com/github/opticyclic/micronaut/demo/Application.java
@@ -0,0 +1,9 @@
+package com.github.opticyclic.micronaut.demo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
